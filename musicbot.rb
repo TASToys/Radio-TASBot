2c8a70da827f961d02aea650651c613e345babcb
MediaMagnet <nathanec@gmail.com>
2018-05-05 20:16:19 -0500

    Music bot works.

--- musicbot.rb
+++ musicbot.rb
@@ -1,21 +1,31 @@
 #!/usr/bin/env ruby
 require 'configatron'
 require 'cinch'
+require 'cinch/commands'
 require 'sqlite3'
 require 'video_info'
 require_relative 'config/configatron/defaults.rb'
 
 VideoInfo.provider_api_keys = { youtube: configatron.yt.api }
 
-
-class Play
+class Request
 	include Cinch::Plugin
+	include Cinch::Commands
 
-	match "play"
+	command :request, {arg1: :string},
+		summery: "Request music with a youtube url."
+	
+	def request(m,arg1)
 
-	def execute(m)
-		contents = m.message
-		m.reply "#{m.user.nick}, #{contents}"
+		@db = SQLite3::Database.open 'db/development.sqlite3'
+		video = VideoInfo.new(arg1)
+		linkvid = arg1
+		linkuser = m.user.nick
+		linktime = Time.now.to_s
+		linkupdt = Time.now.to_s
+		m.reply "#{m.user.nick}, your video \"#{video.title}\" has been added to the queue."
+		@db.execute("INSERT INTO requests (songurl, twitchname, created_at, updated_at) VALUES (?,?,?,?)", [linkvid, linkuser, linktime, linkupdt])
+		@db.close
 	end
 end
 
@@ -24,7 +34,7 @@ bot = Cinch::Bot.new do
 		c.server = "irc.chat.twitch.tv"
 		c.password = configatron.twitch.oauth
 		c.channels = [configatron.twitch.chan]
-		c.plugins.plugins = [Play]
+		c.plugins.plugins = [Request]
 	end
 
 end
