92ca92021461f4cf1514ca811efaca60ea13d470
MediaMagnet <nathanec@gmail.com>
2018-05-05 20:26:32 -0500

    example config added

---
+++ config/configatron/example.defaults.rb
@@ -0,0 +1,14 @@
+# Put all your default configatron settings here.
+#
+# # Example:
+# #   configatron.emails.welcome.subject = 'Welcome!'
+# #   configatron.emails.sales_reciept.subject = 'Thanks for your order'
+# #
+# #   configatron.file.storage = :s3
+# #
+# # youtube api key
+configatron.yt.api = 'YOUR YOUTUBE API KEY'
+configatron.twitch.clientid = 'YOUR TWITCH CLIENT ID'
+configatron.twitch.channel = 'TWITCH CHANNELID YOU WANT TO MONITOR'
+configatron.twitch.oauth = 'YOUR TWITCH OAUTH'
+configatron.twitch.chan = 'THE IRC VERSION OF THE CHANNEL'
