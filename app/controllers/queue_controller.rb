49556469c5ba4566445c3075de2221f7b9a82f79
MediaMagnet <nathanec@gmail.com>
2018-05-11 12:42:48 -0500

    It sorts now

--- app/controllers/queue_controller.rb
+++ app/controllers/queue_controller.rb
@@ -1,12 +1,14 @@
 class QueueController < ApplicationController
 	def queue
-		@requests = Request.all.order(sort_column + " " + sort_direction)
+		#@requests = Request.all
+		@requests = Request.order(sort_column + ' ' + sort_direction)
 	end
+	helper_method :sort_column, :sort_direction
 
 	private
 	
 	def sort_column
-		Request.column_names.include?(params[:sort]) ? params[:sort] : "name"
+		Request.column_names.include?(params[:sort]) ? params[:sort] : "id"
 	end
 	
 	def sort_direction
